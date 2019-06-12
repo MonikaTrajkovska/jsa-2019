9a07662d7fb4ba5f692fa3df75e6ddf20d5e6aba
tatjanaatanasova12 <43668940+tatjanaatanasova12@users.noreply.github.com>
2019-06-12 22:06:21 +0200

    prv cas
    Prv cas node

---
+++ c1/index.js
@@ -0,0 +1,18 @@
+console.log('Hello');
+ime='Bojan';
+var prezime='Bojanovski';
+let vozrast=36;
+
+console.log(`ifjufif ${prezime} nujfcuid`);
+console.log(ime);
+function test(){
+    console.log(ime);
+    console.log(prezime);
+    console.log(vozrast);
+    a=10;
+    var b=20;
+    let c=30;
+}
+test();
+console.log(a);
+console.log(b);
