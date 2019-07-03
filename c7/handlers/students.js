2c2695024eb524a03e11904560b4d01c7cb7b45a
tatjanaatanasova12 <43668940+tatjanaatanasova12@users.noreply.github.com>
2019-07-03 22:08:21 +0200

    cas sedmi update

--- c7/handlers/students.js
+++ c7/handlers/students.js
@@ -1,17 +1,17 @@
 var GetAllStudents = (req, res) => {
     res.send('Hello from /students');
- };
+};
 var GetStudentById = (req, res) => {
     res.send('Get single student with id:' + req.params.id);
 };
-var GetStudentSubjectGrades= (req, res) => {
-        res.send('Get student with id:' + req.params.id + 'grades for subject:' + req.params.sub);
-    };
+var GetStudentSubjectGrades = (req, res) => {
+    res.send('Get student with id:' + req.params.id + 'grades for subject:' + req.params.sub);
+};
 
 
-module.exports={
-   GetAllStudents,
-   GetStudentById,
-   GetStudentSubjectGrades
+module.exports = {
+    GetAllStudents,
+    GetStudentById,
+    GetStudentSubjectGrades
 
 };
--- c7/index.js
+++ c7/index.js
@@ -1,29 +1,30 @@
-var express = require('express');//instalirame express
-var students=require('./handlers/students');
-var calculator= require('./handlers/calculator')
-var api = express();                        
- //pravime instanca na express bibliotekata 
- 
-api.get('/students',students.GetAllStudents);                      
-  // se definira ruta (zakacuvame get method na api i se izvrsuva funkcijata)
- 
+var express = require('express');
+var bodyParser = require('body-parser');
+var students = require('./handlers/students');
+var calculator = require('./handlers/calculator');
+var food = require('./handlers/food');
 
-api.get('/students/:id', students.GetStudentById);                  
-   //kako ke bide vo api.get povikano taka i so req.params. primer reg.params.name/id 
- 
-                          
+var api = express();
+api.use(bodyParser.json());
 
-api.get('/students/:id/grades/:sub', students.GetStudentSubjectGrades );
+api.get('/students', students.GetAllStudents);
+api.get('/students/:id', students.GetStudentByID);
+api.get('/students/:id/grades/:sub', students.GetStudentSubjectGrades);
 
-api.get('/calc/:op/:a/:b',calculator.Calculator);
+api.get('/calc/:op/:a/:b', calculator.Calculator);
+
+api.get('/food', food.GetAllFood);
+api.get('/food/:id', food.GetSingleFood);
+api.post('/food', food.CreateNewFood);
+api.put('/food/:id', food.UpdateFood);
+api.patch('/food/:id', food.PartialUpdateFood);
+api.delete('/food/:id', food.DeleteFood);
 
 
-
-
- api.listen(8080, (err)=>{
-     if(err){
-         console.error(err);
-         return;
-     }
-     console.log('Server started successfully')
- })    // se starta vo terminal so npm start i sekogas se povikuva na toj nacin 
+api.listen(8080, (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log('Server started successfully');
+});
